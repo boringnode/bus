cd0c39335e259cdd30ddfe0aed7013f6b5ca4abc
Romain Lanz <romain.lanz+github@pm.me>
2024-03-08 18:40:43 +0100

    refactor: move the entrypoint at the root
    Closes https://github.com/adonisjs/transmit/issues/4

---
+++ index.ts
@@ -0,0 +1,3 @@
+export { Bus } from './src/bus.js'
+export { BusManager } from './src/bus_manager.js'
+export { defineConfig } from './src/define_config.js'
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export { Bus } from './bus.js'
-export { BusManager } from './bus_manager.js'
-export { defineConfig } from './define_config.js'
