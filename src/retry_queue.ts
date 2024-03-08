98b3ad9f2484b109340190d61152fe9681b20f1f
Romain Lanz <romain.lanz+github@pm.me>
2024-03-08 22:11:27 +0100

    fix(retry_queue): size is a function
    Closes https://github.com/adonisjs/transmit/issues/4

--- src/retry_queue.ts
+++ src/retry_queue.ts
@@ -35,7 +35,7 @@ export class RetryQueue {
   }
 
   size() {
-    return this.#queue.size
+    return this.#queue.size()
   }
 
   async process(handler: (channel: string, message: TransportMessage) => Promise<boolean>) {
